b2bff3daaf2425503c53d84ce0255069239d30fa
huntj0n <hunt.jon19@gmail.com>
2020-11-17 15:10:05 -0700

    banner.js initial setup and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
+
+  //mobile device vh
+  useEffect(() => {
+    let vh = window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }, [])
+
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
